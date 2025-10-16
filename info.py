4fbcbf25c1f17f0cebe0d823fbcf9fb0d3aee59b
furkangugmol4 <furkangumgol4@gamil.com>
2025-10-16 13:58:27 +0530

    save

---
+++ info.py
@@ -0,0 +1,10 @@
+# Ask for user details
+name = input("Enter your name: ")
+usn = input("Enter your USN: ")
+division = input("Enter your division: ")
+
+# Print the entered details
+print("\n--- User Details ---")
+print(f"Name: {name}")
+print(f"USN: {usn}")
+print(f"Address: {division}")
